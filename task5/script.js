b89efce4dcf0c7278ed76b7bbd273ca1b5482ae8
shinsenman <takahiro-kawasaki@cybozu.co.jp>
2020-06-19 16:56:20 +0900

    task5 done

--- task5/script.js
+++ task5/script.js
@@ -2,59 +2,42 @@
 
 // jQueryを使う時のお約束
 $(document).ready(() => {
-  // 各変数宣言
 
   // 「住所を検索」ボタンを押したときにイベントが発火
   $('#zipSearch').on('click', () => {
     // 入力された郵便番号を変数に格納
     var zipCode = $('#zipCode').val();
-    console.log(zipCode);
 
-    // apiを叩き住所を取得する
+    // APIの実行に必要な情報を変数に格納
     var apiKey = 'dj00aiZpPUJsQ3ZlMHoxU0pKViZzPWNvbnN1bWVyc2VjcmV0Jng9ZWE-';
-
-    var url = 'https://map.yahooapis.jp/search/zip/V1/zipCodeSearch?query=' + zipCode + '&appid=' + apiKey + '&o=json&callback=showResult';
-
-    console.log(url);
-
-    // function callJSONP(url){
-    //   var target = document.createElement('script');
-    //   target.charset = 'utf-8';
-    //   target.src = url;
-    //   document.body.appendChild(target);
-    // };
-    // var results = callJSONP(url);
-
-    // function showResult( result ) {
-    //   if ( result.Count > 0 ) {
-    //     alert( result.Count + "件の結果が見つかりました。\n" +
-    //           result.Item[0].Title + "の住所は" + result.Item[0].Address + "です。" );
-    //   } else {
-    //     alert( "検索結果が見つかりませんでした。" );
-    //   }
-    // }
-
-    // console.log(results);
-
+    var url = 'https://map.yahooapis.jp/search/zip/V1/zipCodeSearch?query=' + zipCode + '&appid=' + apiKey + '&output=json';
+    
+    // APIを実行
     $.ajax({
       type: 'GET',
       url: url,
       dataType: 'jsonp'
     }).done((resp) => {
-      console.log(resp);
+      // レスポンスデータから住所、緯度経度、最寄りの駅を抽出・成形
+      var addressData = resp.Feature[0].Property;
+      var geometryData = resp.Feature[0].Geometry.Coordinates.split(',');
+      var stationData = addressData.Station;
+      var nearStations = new String();
+
+      for (var i = 0; i < stationData.length; i++) {
+        var nearStation = stationData[i].Name + '(' + stationData[i].Railway + '),';
+        nearStations += nearStation;
+      };
+
+      // 表に書き込む
+      $('.addressTable').find('td')[0].textContent = addressData.Address;   
+      $('.addressTable').find('td')[1].textContent = geometryData[0];
+      $('.addressTable').find('td')[2].textContent = geometryData[1];
+      $('.addressTable').find('td')[3].textContent = nearStations.slice(0, -1);
     }).fail((err) => {
       console.log(err);
     });
-
-    // 表に書き込む
-    $('.addressTable').find('td')[0].textContent = '○○県○○市';   
-    $('.addressTable').find('td')[1].textContent = 'keido';
-    $('.addressTable').find('td')[2].textContent = 'ido';
-    $('.addressTable').find('td')[3].textContent = '○○駅'
   });
-
-
-
 });
 
 
