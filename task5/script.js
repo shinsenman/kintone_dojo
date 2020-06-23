e38193dd731966479fedc4d5dbb45f29eb232b7c
shinsenman <takahiro-kawasaki@cybozu.co.jp>
2020-06-23 10:55:19 +0900

    a few mistakes fixed

--- task5/script.js
+++ task5/script.js
@@ -6,39 +6,45 @@ $(document).ready(() => {
   // 「住所を検索」ボタンを押したときにイベントが発火
   $('#zipSearch').on('click', () => {
     // 入力された郵便番号を変数に格納
-    var zipCode = $('#zipCode').val();
+    const zipCode = $('#zipCode').val();
 
     // APIの実行に必要な情報を変数に格納
-    var apiKey = 'dj00aiZpPUJsQ3ZlMHoxU0pKViZzPWNvbnN1bWVyc2VjcmV0Jng9ZWE-';
-    var url = 'https://map.yahooapis.jp/search/zip/V1/zipCodeSearch?query=' + zipCode + '&appid=' + apiKey + '&output=json';
-    
+    const apiKey = 'dj00aiZpPUJsQ3ZlMHoxU0pKViZzPWNvbnN1bWVyc2VjcmV0Jng9ZWE-';
+    const url = `https://map.yahooapis.jp/search/zip/V1/zipCodeSearch?query=${zipCode}&appid=${apiKey}&output=json`;
+
     // APIを実行
     $.ajax({
       type: 'GET',
       url: url,
       dataType: 'jsonp'
     }).done((resp) => {
-      // レスポンスデータから住所、緯度経度、最寄りの駅を抽出・成形
-      var addressData = resp.Feature[0].Property;
-      var geometryData = resp.Feature[0].Geometry.Coordinates.split(',');
-      var stationData = addressData.Station;
-      var nearStations = new String();
+      if (resp.Feature) {
+        if ($('.errMsg')[0]) {
+          $('.errMsg').remove();
+        }
+        // レスポンスデータから住所、緯度経度、最寄りの駅を抽出・成形
+        const addressData = resp.Feature[0].Property;
+        const geometryData = resp.Feature[0].Geometry.Coordinates.split(',');
+        const stationData = addressData.Station;
+        let nearStations = "";
 
-      for (var i = 0; i < stationData.length; i++) {
-        var nearStation = stationData[i].Name + '(' + stationData[i].Railway + '),';
-        nearStations += nearStation;
-      };
+        // 最寄り駅を1行に連結
+        for (let i = 0; i < stationData.length; i++) {
+          const nearStation = `${stationData[i].Name}(${stationData[i].Railway}),`;
+          nearStations += nearStation;
+        };
 
-      // 表に書き込む
-      $('.addressTable').find('td')[0].textContent = addressData.Address;   
-      $('.addressTable').find('td')[1].textContent = geometryData[0];
-      $('.addressTable').find('td')[2].textContent = geometryData[1];
-      $('.addressTable').find('td')[3].textContent = nearStations.slice(0, -1);
+        // 表に書き込む
+        $('.addressTable').find('td')[0].textContent = addressData.Address;
+        $('.addressTable').find('td')[1].textContent = geometryData[0];
+        $('.addressTable').find('td')[2].textContent = geometryData[1];
+        $('.addressTable').find('td')[3].textContent = nearStations.slice(0, -1);
+      } else if (!$('.errMsg')[0]) {
+        $('.searchField').append('<p class="errMsg" style="color:#ff0000; background-color:#ffcccc; width:50%; margin:0 auto">入力された郵便番号は存在しません。<br>もう一度確認して再入力してください。</p>');
+      }
     }).fail((err) => {
       console.log(err);
+      $('.searchField').append('<p>検索システムが動作していません。管理者にお問い合わせください。</p>');
     });
   });
 });
-
-
-
